cdb4b020f8e4385a61e6b0f2f5f14e758726f052
Muhammad Usama <162746442+MUsama111@users.noreply.github.com>
2024-03-12 23:39:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var PersonName = "Ahmed";
+console.log("Hello ".concat(PersonName, " would you like to learn some Python today?"));
+var PersonName1 = "Haris Ali";
+var PersonName2 = "Sara Khan";
+var PersonName3 = "Kiran and Usama";
+console.log("".concat(PersonName1, " how old are you?"));
+console.log("".concat(PersonName2, " is a nice girl and she loves to read books"));
+console.log("".concat(PersonName3, " they both are good friends?"));
---
+++ app.ts
@@ -0,0 +1,8 @@
+let PersonName: String= "Ahmed";
+console.log(`Hello ${PersonName} would you like to learn some Python today?`);
+let PersonName1: String= "Haris Ali";
+let PersonName2: string= "Sara Khan";
+let PersonName3: String= "Kiran and Usama";
+console.log(`${PersonName1} how old are you?`);
+console.log(`${PersonName2} is a nice girl and she loves to read books`);
+console.log(`${PersonName3} they both are good friends?`);
